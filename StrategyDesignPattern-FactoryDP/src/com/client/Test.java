b8e20d4c385eba30253168e2b4b89a51b1923a6b
Ravi-Kushwah3 <ravikushwah6409@gmail.com>
2025-02-03 22:43:03 +0530

    adding properties file to take dependent class

--- StrategyDesignPattern-FactoryDP/src/com/client/Test.java
+++ StrategyDesignPattern-FactoryDP/src/com/client/Test.java
@@ -4,11 +4,18 @@ import com.component.Flipkart;
 import com.factory.FlipkartFactory;
 
 public class Test {
-	public static void main(String[] args) {
-		FlipkartFactory factory = new FlipkartFactory();
-		Flipkart fk = factory.getInstance("bDart");
-		String message = fk.shopping(new String[] {"Apple","Orange","Banana","Mango"}, new double[] {2000.0,500.0,8000.0,562.0});
-		System.out.println(message);
+	public static void main(String[] args) throws Exception {
+		FlipkartFactory factory = new FlipkartFactory();	
+		Flipkart fk;
+		try {
+			fk = factory.getInstance();
+			String message = fk.shopping(new String[] {"Apple","Orange","Banana","Mango"}, new double[] {2000.0,500.0,8000.0,562.0});
+			System.out.println(message);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
 	}
 
 }
--- StrategyDesignPattern-FactoryDP/src/com/factory/FlipkartFactory.java
+++ StrategyDesignPattern-FactoryDP/src/com/factory/FlipkartFactory.java
@@ -1,24 +1,50 @@
 package com.factory;
 
-import com.component.BlueDart;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.util.Properties;
+
 import com.component.Courier;
-import com.component.DHL;
 import com.component.Flipkart;
 
 public class FlipkartFactory {
+	private static Properties prop;
 	public FlipkartFactory() {
 	 System.out.println("FlipkartFactory.FlipkartFactory()");
 	}
-   public Flipkart getInstance(String courierType) {
+	static {
+		try {
+			InputStream in = new FileInputStream("src/com/commons/info.properties");
+			prop = new Properties();
+			prop.load(in);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Flipkart getInstance(/* String courierType */) throws Exception {
 	   Courier courier = null;
-	   if(courierType == "dhl") {
-		   courier = new DHL();
-	   }else if(courierType == "bDart") {
-		   courier = new BlueDart();
-	   }
-	   else {
-		   throw new IllegalArgumentException("Invalid Courier type");
-	   }
+//	   if(courierType == "dhl") {
+//		   courier = new DHL();
+//	   }else if(courierType == "bDart") {
+//		   courier = new BlueDart();
+//	   }
+//	   else {
+//		   throw new IllegalArgumentException("Invalid Courier type");
+//	   }
+	  String courierType = prop.getProperty("courier.name");
+	   
+	   Class c = Class.forName(courierType);
+	   try {
+		   // deprecated method 
+		//courier = (Courier) c.newInstance();
+		   Constructor[] cons = c.getDeclaredConstructors();
+		   //0-param constructer
+		   courier =(Courier) cons[0].newInstance();
+	} catch (Exception e ) {
+		e.printStackTrace();
+	}
 	   Flipkart flk = new Flipkart();
 	   flk.setCourier(courier);
 	   return flk;
